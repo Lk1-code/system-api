6304f47e0f16a964ca50d953de80030978be7c26
Lucas Mariano Da Silva <lucasm1593@gmail.com>
2024-08-01 22:20:38 -0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require('../config/config');
+
+const User = sequelize.define('User',{
+    username: {
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    loginuser:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+    },
+    password:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    active:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false,
+    },
+},{
+    timestamps:false,
+});
+
+
+module.exports = User;
